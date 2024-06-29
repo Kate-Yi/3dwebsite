cd7c652f74be31fe7723b7aca997fc4c34aee362
Kate Yi <127848068+Kate-Yi@users.noreply.github.com>
2024-06-29 20:05:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const $ = (queryString) => document.querySelectorAll(queryString);
 
-  const colors = ['#0369C2', '#4DBE46', '#D7DA3F', '#8680C6', 'white', 'black', 'gray', '#F2393D'];
-  let colorIndex = 0;
-  const framesPerColor = 600; 
-  let frameCounter = 0;
+  const shiftHue = (hue) => (hue + 1) % 360;
+
+  let hue = 0;
 
   const animate = () => {
-    const color = colors[colorIndex];
+    hue = shiftHue(hue);
+    const color = `hsl(${hue}, 100%, 50%)`;
     const spheres = $('a-sphere');
 
     spheres.forEach(sphere => {
       // Set color
       sphere.setAttribute('color', color);
     });
-
-    // Update frame counter
-    frameCounter++;
-
-    // Move to the next color after the specified number of frames
-    if (frameCounter >= framesPerColor) {
-      frameCounter = 0;
-      colorIndex = (colorIndex + 1) % colors.length;
-    }
 
     requestAnimationFrame(animate);
   };
