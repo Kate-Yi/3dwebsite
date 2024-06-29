3ce1097ebe85094b708d799f2c8b959c972997ba
Kate Yi <127848068+Kate-Yi@users.noreply.github.com>
2024-06-29 14:46:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-$ = (queryString) => document.querySelectorAll(queryString);
+const $ = (queryString) => document.querySelectorAll(queryString);
 
 const shiftHue = (hue) => (hue + 1) % 360;
+const amplitude = 0.5; 
+const frequency = 0.02;
 
 let hue = 0;
+let time = 0;
 
 const animate = () => {
-  hue = shiftHue(hue)
+  hue = shiftHue(hue);
   const color = `hsl(${hue}, 100%, 50%)`;
-  $('a-sphere').setAttribute('color', color);
+  const spheres = $('a-sphere');
+  spheres.forEach(sphere => {
+    sphere.setAttribute('color', color);
+    const position = sphere.getAttribute('position');
+    const newY = position.y + amplitude * Math.sin(time * frequency);
+    sphere.setAttribute('position', `${position.x} ${newY} ${position.z}`);
+  });
+  time++;
   requestAnimationFrame(animate);
 };
 
